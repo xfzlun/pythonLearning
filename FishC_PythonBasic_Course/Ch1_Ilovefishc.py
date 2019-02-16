56cb641b7740d89574f3bd64000faf4aec7b6f57
julylun@lenovo <xfz79@foxmail.com>
2019-02-16 23:34:23 +0800

    Ch1_modified

--- FishC_PythonBasic_Course/Ch1_Ilovefishc.py
+++ FishC_PythonBasic_Course/Ch1_Ilovefishc.py
@@ -6,3 +6,27 @@ FileName:Ch1_Ilovefishc.py
 @DATE:2019-Feb-16;Sat
 @TIME:10:19:22
 '''
+
+'''
+What is IDLE?
+IDLE is a shell of Python, 是一个通过键入文本与程序交互的路径，例如Windows的cmd, Linux的terminal(bash)
+>>>  IDLE中的这个提示符表示等待键入命令的提示符
+
+
+print语句
+'''
+
+#试着在IDLE中输入 以下语句
+print('I love fishc.com')
+
+'''
+错误示范：
+试着在IDLE中输入 以下语句
+print "I love fishc.com"  -- 这是python2的语法，不适用python3
+在IDLE中，回到前一个语句的快捷键 ALT+N
+到下一个语句  ALT+P
+'''
+
+
+
+
