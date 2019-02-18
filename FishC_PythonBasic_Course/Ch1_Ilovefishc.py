aa8c632e6548483508838c5be586f416010316e7
xfzlun <xfz79@foxmail.com>
2019-02-18 16:48:42 +0800

    20190218Lesson

--- FishC_PythonBasic_Course/Ch1_Ilovefishc.py
+++ FishC_PythonBasic_Course/Ch1_Ilovefishc.py
@@ -23,11 +23,26 @@ print('I love fishc.com')
 错误示范：
 试着在IDLE中输入 以下语句
 print "I love fishc.com"  -- 这是python2的语法，不适用python3
-在IDLE中，回到前一个语句的快捷键 ALT+N
+
+在IDLE中，
+回到前一个语句的快捷键 ALT+N
 到下一个语句  ALT+P
 '''
 
-# print 8次
+'''
+print()括號中用引號括住的表示是字符串，如果是要輸入用来计算用的整型，则不需要加入引号‘’，另外，字符串也可以做相加或乘法如以下几个例子：
+>>>print(5+3)
+>>>5+3
+>>>1234567890987654321*9876543210123456789
+>>>print("well water" + "river" )
+'''
+
+print('I love fishc.com' * 8) #把I love fishC.com打印8次
+print('I love fishc.com\n' * 8) #把I love fishc.com 加上回車以後打印8次
+
+
+
+
 
 
 
--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python
 #_*_coding: utf-8 _*_
 '''
-@uthor:aLuren
-Copyright(c)2018 SZ_MB-QTC
+@uthor:aLun@july
+Copyright(c)2019 SZ_MB-QTC
 FileName:hello.py
-@DATE:2018-Nov-21;Wed
-@TIME:21:55:28
+@DATE:2019-Feb-18;Mon
+@TIME:10:58:03
 '''
